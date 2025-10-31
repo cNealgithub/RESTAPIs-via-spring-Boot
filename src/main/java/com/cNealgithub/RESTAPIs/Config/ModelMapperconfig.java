6f6318334ede15cf73f222d51c718bfcd7a990d4
Neal Chakravarty <nealchakravarty930@gmail.com>
2025-10-31 09:55:34 +0530

    added model mapper dependency and getstudentbyid() methd

---
+++ src/main/java/com/cNealgithub/RESTAPIs/Config/ModelMapperconfig.java
@@ -0,0 +1,13 @@
+package com.cNealgithub.RESTAPIs.Config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperconfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
